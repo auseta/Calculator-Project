501a032e6e20d791782b56eb671c90f46d65daaf
Augusto Seta <alseta97@gmail.com>
2022-09-13 15:56:58 -0300

    major changes in all the files

--- index.js
+++ index.js
@@ -1,30 +1,124 @@
-function add(num1, num2) {
-  return num1 + num2
-}
-
-function subtract(num1, num2) {
-  return num1 - num2
-}
-
-function multiply(num1, num2) {
-  return num1 * num2
-}
-
-function divide(num1, num2) {
-  return num1 / num2
-}
-
-function operate (operator, num1, num2) {
-  if (operator === "+") {
-    return add(num1, num2)
-  } else if (operator === "-") {
-    return subtract(num1, num2)
-  } else if (operator === "*") {
-    return multiply(num1, num2)
-  } else if (operator === "/") {
-    return divide(num1, num2)
+function handleNumber(number) {
+  if (previousValue !== "" && currentValue !== "" && operator !== "") {
+    previousValue = "";
+    currentDisplayNumber.textContent = currentValue;
+  }
+  if (currentValue.length < 8) {
+    currentValue += number;
+    currentDisplayNumber.textContent = currentValue;
   }
 }
 
-let result = operate("/", 5, 8)
-console.log(result);
+function handleOperator(op) {
+  if (previousValue === "") {
+    previousValue = currentValue;
+    checkOperator(op)
+  } else if (currentValue === "") {
+    checkOperator(op)
+  } else {
+    operate();
+    operator = op;
+    currentDisplayNumber.textContent = "0";
+    previousDisplayNumber.textContent = `${previousValue} ${operator}`
+  }
+}
+
+function checkOperator(op) {
+  operator = op;
+  previousDisplayNumber.textContent = `${previousValue} ${operator}`;
+  currentDisplayNumber.textContent = "0";
+  currentValue = "";
+}
+
+function operate () {
+  previousValue = +previousValue;
+  currentValue = +currentValue;
+
+  if (operator === "+") {
+    previousValue +=currentValue;
+  } else if (operator === "-") {
+    previousValue -= currentValue;
+  } else if (operator === "*") {
+    previousValue *= currentValue;
+  } else if (operator === "/") {
+    if (currentValue === 0) {
+      previousValue = "Error";
+      displayResult()
+      return
+    }
+    previousValue /= currentValue;
+  }
+  previousValue = roundNumber(previousValue);
+  previousValue = previousValue.toString();
+  displayResult()
+}
+
+function roundNumber(num) {
+  return Math.round(num * 100000) / 100000
+}
+
+function displayResult() {
+  if (previousValue.length < 8) {
+    currentDisplayNumber.textContent = previousValue;
+  } else {
+    currentDisplayNumber.textContent = `${previousValue.slice(0, 8)}...`
+  }
+  previousDisplayNumber.textContent = "";
+  operator = "";
+  currentValue = "";
+}
+
+function clearCalculator() {
+  currentValue = "";
+  previousValue = "";
+  operator = "";
+  previousDisplayNumber.textContent = "0";
+  currentDisplayNumber.textContent = "";
+}
+
+
+
+function addDecimal() {
+  if (!currentValue.includes(".")) {
+    currentValue +="."
+    currentDisplayNumber.textContent = currentValue
+  }
+}
+
+let currentValue = "";
+let previousValue = "";
+let operator = "";
+
+const previousDisplayNumber = document.querySelector(".calculator-operation");
+const currentDisplayNumber = document.querySelector(".calculator-result");
+
+const equal = document.querySelector("#equal");
+const decimal = document.querySelector("#decimal");
+const clear = document.querySelector("#clear");
+const operators = document.querySelectorAll("button[data-button-type='operator']")
+
+const numberButtons = document.querySelectorAll("button[data-button-type='number']")
+
+numberButtons.forEach( button => {
+  button.addEventListener("click", (e) => {
+    handleNumber(e.target.value)
+  })
+})
+
+operators.forEach( button => {
+  button.addEventListener("click", (e) => {
+    handleOperator(e.target.value);
+  })
+})
+
+equal.addEventListener("click", () => {
+  if (previousValue !== "" && currentValue !== "") {
+    operate()
+  }
+})
+
+clear.addEventListener("click", clearCalculator)
+
+decimal.addEventListener("click", () => {
+  addDecimal()
+})
